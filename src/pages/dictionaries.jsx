974c37d6ab09da6e7fd367b199f269a385463265
aSDD <106367851+aAlfonsoo@users.noreply.github.com>
2025-02-16 21:50:02 +0800

    modified:   src/pages/cards.jsx 	modified:   src/pages/dictionaries.jsx 	modified:   src/pages/spelling.jsx

--- src/pages/dictionaries.jsx
+++ src/pages/dictionaries.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { confetti, dictionaryTutorial, goBack, pictureDictionaries, excellent, verygood, good, okay, number1, number2, number3, number4, number5, number6, number7, color1, color2, color3, color4, color7, color10, color11, animal1, animal2, animal3, animal4, animal5, animal6, animal12, fruitVeg1, fruitVeg2, fruitVeg4, fruitVeg5, fruitVeg6, fruitVeg7, fruitVeg10, genVocab1, genVocab2, genVocab3, genVocab6, genVocab7, genVocab9, genVocab11 } from "../assets/images";
+import { useNavigate, useLocation } from "react-router-dom";
+import { confetti, dictionaryTutorial, goBack, pictureDictionaries, excellent, verygood, good, okay, number1, number2, number3, number4, number5, number6, number7, number8, number9, number10, color1, color2, color3, color4, color5, color6, color7, color8, color9, color10, color11, animal1, animal2, animal3, animal4, animal5, animal6, animal7, animal8, animal9, animal10, animal11, animal12, animal13, animal14, animal15, animal16, fruitVeg1, fruitVeg2, fruitVeg3, fruitVeg4, fruitVeg5, fruitVeg6, fruitVeg7, fruitVeg8, fruitVeg9, fruitVeg10, fruitVeg11, fruitVeg12, fruitVeg13, fruitVeg14, fruitVeg15, genVocab1, genVocab2, genVocab3, genVocab4, genVocab5, genVocab6, genVocab7, genVocab8, genVocab9, genVocab10, genVocab11, genVocab12, genVocab13, genVocab14, genVocab15, genVocab16, genVocab17, } from "../assets/images";
 import "../App.modules.css";
 import { bgMusic, numberS1, numberS2, numberS3, numberS4, numberS5, numberS6, numberS7, numberS8, numberS9, numberS10, phraseS1, phraseS2, phraseS3, phraseS4, phraseS5, phraseS6, phraseS7, phraseS8, phraseS9, phraseS10, phraseS11, phraseS12, phraseS13, phraseS14, phraseS15, phraseS16, phraseS17, phraseS18, phraseS19, animalS1, animalS2, animalS3, animalS4, animalS5, animalS6, animalS7, animalS8, animalS9, animalS10, animalS11, animalS12, animalS13, animalS14, animalS15, animalS16, colorS1, colorS2, colorS3, colorS4, colorS5, colorS6, colorS7, colorS8, colorS9, colorS10, colorS11, fruitVegS1, fruitVegS2, fruitVegS3, fruitVegS4, fruitVegS5, fruitVegS6, fruitVegS7, fruitVegS8, fruitVegS9, fruitVegS10, fruitVegS11, fruitVegS12, fruitVegS13, fruitVegS14, fruitVegS15, genVocabS1, genVocabS2, genVocabS3, genVocabS4, genVocabS5, genVocabS6, genVocabS7, genVocabS8, genVocabS9, genVocabS10, genVocabS11, genVocabS12, genVocabS13, genVocabS14, genVocabS15, genVocabS16, genVocabS17 } from "../assets/musics";
 
@@ -10,36 +10,93 @@ function Dictionaries() {
         navigate(-1);
     };
 
-    const pictureDictionary = [
-        { id: 1, word: "Metung", image: number1, sound: numberS1, choices: ["Atlu", "Apat", "Adwa", "Metung"], correctAnswer: "Metung" },
-        { id: 2, word: "Adwa", image: number2, sound: numberS2, choices: ["Adwa", "Metung", "Atlu", "Apat"], correctAnswer: "Adwa" },
-        { id: 3, word: "Atlu", image: number3, sound: numberS3, choices: ["Apat", "Adwa", "Atlu", "Metung"], correctAnswer: "Atlu" },
-        { id: 4, word: "Apat", image: number4, sound: numberS4, choices: ["Apat", "Metung", "Atlu", "Adwa"], correctAnswer: "Apat" },
-        { id: 5, word: "Lima", image: number5, sound: numberS5, choices: ["Walu", "Anam", "Pito", "Lima"], correctAnswer: "Lima" },
-        { id: 6, word: "Anam", image: number6, sound: numberS6, choices: ["Anam", "Pito", "Walu", "Lima"], correctAnswer: "Anam" },
-        { id: 7, word: "Pito", image: number7, sound: numberS7, choices: ["Siyam", "Pito", "Anam", "Walu"], correctAnswer: "Pito" },
-        { id: 8, word: "Mairo", image: color1, sound: colorS1, choices: ["Malutu", "Tayum", "Mairo", "Aluntiang"], correctAnswer: "Mairo" },
-        { id: 9, word: "Tayum", image: color2, sound: colorS2, choices: ["Mairo", "Tayum", "Malutu", "Aluntiang"], correctAnswer: "Tayum" },
-        { id: 10, word: "Aluntiang", image: color3, sound: colorS3, choices: ["Malutu", "Tayum", "Aluntiang", "Kuliawan"], correctAnswer: "Aluntiang" },
-        { id: 11, word: "Kuliawan", image: color4, sound: colorS4, choices: ["Aluntiang", "Kuliawan", "Malutu", "Mairo"], correctAnswer: "Kuliawan" },
-        { id: 12, word: "Ubi", image: color7, sound: colorS7, choices: ["Mairo", "Malutu", "Ubi", "Tayum"], correctAnswer: "Ubi" },
-        { id: 13, word: "Malutu", image: color10, sound: colorS10, choices: ["Malutu", "Tayum", "Kuliawan", "Aluntiang"], correctAnswer: "Malutu" },
-        { id: 14, word: "Tuling", image: color11, sound: colorS11, choices: ["Kuliawan", "Aluntiang", "Malutu", "Tuling"], correctAnswer: "Tuling" },
-        { id: 15, word: "Asu", image: animal1, sound: animalS1, choices: ["Dagis", "Baka", "Manuk", "Asu"], correctAnswer: "Asu" },
-        { id: 16, word: "Baka", image: animal2, sound: animalS2, choices: ["Manuk", "Asu", "Dagis", "Baka"], correctAnswer: "Baka" },
-        { id: 17, word: "Manuk", image: animal3, sound: animalS3, choices: ["Manuk", "Asu", "Baka", "Dagis"], correctAnswer: "Manuk" },
-        { id: 18, word: "Dagis", image: animal4, sound: animalS4, choices: ["Baka", "Asu", "Manuk", "Dagis"], correctAnswer: "Dagis" },
-        { id: 19, word: "Babi", image: animal5, sound: animalS5, choices: ["Dagis", "Baka", "Babi", "Asu"], correctAnswer: "Babi" },
-        { id: 20, word: "Dangki", image: animal6, sound: animalS6, choices: ["Babi", "Dangki", "Asu", "Manuk"], correctAnswer: "Dangki" },
-        { id: 21, word: "Leun", image: animal12, sound: animalS12, choices: ["Baka", "Leun", "Dagis", "Manuk"], correctAnswer: "Leun" },
-        { id: 22, word: "Ubas", image: fruitVeg1, sound: fruitVegS1, choices: ["Kalamunding", "Saging", "Ubas", "Sibuyas"], correctAnswer: "Ubas" },
-        { id: 23, word: "Kalamunding", image: fruitVeg2, sound: fruitVegS2, choices: ["Kalamunding", "Ubas", "Saging", "Sibuyas"], correctAnswer: "Kalamunding" },
-        { id: 24, word: "Sibuyas", image: fruitVeg4, sound: fruitVegS4, choices: ["Saging", "Kalamunding", "Ubas", "Sibuyas"], correctAnswer: "Sibuyas" },
-        { id: 25, word: "Peras", image: fruitVeg5, sound: fruitVegS5, choices: ["Saging", "Mais", "Peras", "Ubas"], correctAnswer: "Peras" },
-        { id: 26, word: "Apol", image: fruitVeg6, sound: fruitVegS6, choices: ["Mais", "Saging", "Peras", "Apol"], correctAnswer: "Apol" },
-        { id: 27, word: "Saging", image: fruitVeg7, sound: fruitVegS7, choices: ["Peras", "Apol", "Mais", "Saging"], correctAnswer: "Saging" },
-        { id: 28, word: "Mais", image: fruitVeg10, sound: fruitVegS10, choices: ["Saging", "Mais", "Apol", "Peras"], correctAnswer: "Mais" }
-    ];
+    const location = useLocation(); // Get the category from navigation state
+    const { category } = location.state || {};
+
+    const pictureDictionaryByCategory = {
+        numbers: [
+            { id: 1, word: "Metung", image: number1, sound: numberS1, choices: ["Atlu", "Apat", "Adwa", "Metung"], correctAnswer: "Metung" },
+            { id: 2, word: "Adwa", image: number2, sound: numberS2, choices: ["Adwa", "Metung", "Atlu", "Apat"], correctAnswer: "Adwa" },
+            { id: 3, word: "Atlu", image: number3, sound: numberS3, choices: ["Apat", "Adwa", "Atlu", "Metung"], correctAnswer: "Atlu" },
+            { id: 4, word: "Apat", image: number4, sound: numberS4, choices: ["Apat", "Metung", "Atlu", "Adwa"], correctAnswer: "Apat" },
+            { id: 5, word: "Lima", image: number5, sound: numberS5, choices: ["Walu", "Anam", "Pito", "Lima"], correctAnswer: "Lima" },
+            { id: 6, word: "Anam", image: number6, sound: numberS6, choices: ["Anam", "Pito", "Walu", "Lima"], correctAnswer: "Anam" },
+            { id: 7, word: "Pito", image: number7, sound: numberS7, choices: ["Siyam", "Pito", "Anam", "Walu"], correctAnswer: "Pito" },
+            { id: 8, word: "Walu", image: number8, sound: numberS8, choices: ["Walu", "Pito", "Siyam", "Anam"], correctAnswer: "Walu" },
+            { id: 9, word: "Siam", image: number9, sound: numberS9, choices: ["Siyam", "Walu", "Pito", "Apat"], correctAnswer: "Siyam" },
+            { id: 10, word: "Apulu", image: number10, sound: numberS10, choices: ["Lima", "Apulu", "Anam", "Siyam"], correctAnswer: "Apulu" }
+        ],        
+        colours: [
+            { id: 1, word: "Mairo", image: color1, sound: colorS1, choices: ["Mairo", "Tayum", "Aluntiang", "Kuliawan"], correctAnswer: "Mairo" },
+            { id: 2, word: "Tayum", image: color2, sound: colorS2, choices: ["Tayum", "Mairo", "Komanggi", "Ubi"], correctAnswer: "Tayum" },
+            { id: 3, word: "Aluntiang", image: color3, sound: colorS3, choices: ["Dilo", "Diwa", "Aluntiang", "Tayum"], correctAnswer: "Aluntiang" },
+            { id: 4, word: "Kuliawan", image: color4, sound: colorS4, choices: ["Kuliawan", "Malutu", "Komanggi", "Ubi"], correctAnswer: "Kuliawan" },
+            { id: 5, word: "Diwa", image: color5, sound: colorS5, choices: ["Abu", "Dilo", "Diwa", "Tayum"], correctAnswer: "Diwa" },
+            { id: 6, word: "Komanggi", image: color6, sound: colorS6, choices: ["Komanggi", "Ubi", "Tayum", "Dilo"], correctAnswer: "Komanggi" },
+            { id: 7, word: "Ubi", image: color7, sound: colorS7, choices: ["Malutu", "Dilo", "Ubi", "Mairo"], correctAnswer: "Ubi" },
+            { id: 8, word: "Abu", image: color8, sound: colorS8, choices: ["Tayum", "Diwa", "Abu", "Aluntiang"], correctAnswer: "Abu" },
+            { id: 9, word: "Dilo", image: color9, sound: colorS9, choices: ["Dilo", "Komanggi", "Ubi", "Kuliawan"], correctAnswer: "Dilo" },
+            { id: 10, word: "Malutu", image: color10, sound: colorS10, choices: ["Malutu", "Tayum", "Aluntiang", "Dilo"], correctAnswer: "Malutu" },
+            { id: 11, word: "Tuling", image: color11, sound: colorS11, choices: ["Abu", "Malutu", "Tuling", "Mairo"], correctAnswer: "Tuling" }
+        ],
+        animals: [
+            { id: 1, word: "Asu", image: animal1, sound: animalS1, choices: ["Asu", "Baka", "Manuk", "Dagis"], correctAnswer: "Asu" },
+            { id: 2, word: "Baka", image: animal2, sound: animalS2, choices: ["Baka", "Asu", "Manuk", "Tupa"], correctAnswer: "Baka" },
+            { id: 3, word: "Manuk", image: animal3, sound: animalS3, choices: ["Manuk", "Dagis", "Baka", "Asu"], correctAnswer: "Manuk" },
+            { id: 4, word: "Dagis", image: animal4, sound: animalS4, choices: ["Baboy", "Dagis", "Asu", "Manuk"], correctAnswer: "Dagis" },
+            { id: 5, word: "Babi", image: animal5, sound: animalS5, choices: ["Babi", "Dangki", "Dagis", "Tugak"], correctAnswer: "Babi" },
+            { id: 6, word: "Dangki", image: animal6, sound: animalS6, choices: ["Tugak", "Dangki", "Talubang", "Babi"], correctAnswer: "Dangki" },
+            { id: 7, word: "Talubang", image: animal7, sound: animalS7, choices: ["Elepanti", "Talubang", "Tugak", "Babi"], correctAnswer: "Talubang" },
+            { id: 8, word: "Elepanti", image: animal8, sound: animalS8, choices: ["Elepanti", "Talubang", "Kambing", "Kabayo"], correctAnswer: "Elepanti" },
+            { id: 9, word: "Tugak", image: animal9, sound: animalS9, choices: ["Tugak", "Talubang", "Kabayo", "Leun"], correctAnswer: "Tugak" },
+            { id: 10, word: "Kambing", image: animal10, sound: animalS10, choices: ["Kambing", "Kabayo", "Tupa", "Baka"], correctAnswer: "Kambing" },
+            { id: 11, word: "Kabayu", image: animal11, sound: animalS11, choices: ["Kabayu", "Leun", "Limpasut", "Balyena"], correctAnswer: "Kabayu" },
+            { id: 12, word: "Leun", image: animal12, sound: animalS12, choices: ["Leun", "Tupa", "Babi", "Limpasut"], correctAnswer: "Leun" },
+            { id: 13, word: "Limpasut", image: animal13, sound: animalS13, choices: ["Limpasut", "Balyena", "Tupa", "Ubigan"], correctAnswer: "Limpasut" },
+            { id: 14, word: "Tupa", image: animal14, sound: animalS14, choices: ["Tupa", "Ubigan", "Leun", "Kabayu"], correctAnswer: "Tupa" },
+            { id: 15, word: "Ubigan", image: animal15, sound: animalS15, choices: ["Ubigan", "Balyena", "Tupa", "Limpasut"], correctAnswer: "Ubigan" },
+            { id: 16, word: "Balyena", image: animal16, sound: animalS16, choices: ["Balyena", "Tupa", "Ubigan", "Leun"], correctAnswer: "Balyena" }
+        ],
+        fruitsVeg: [
+            { id: 1, word: "Ubas", image: fruitVeg1, sound: fruitVegS1, choices: ["Ubas", "Kalamunding", "Peras", "Sibuyas"], correctAnswer: "Ubas" },
+            { id: 2, word: "Kalamunding", image: fruitVeg2, sound: fruitVegS2, choices: ["Kalamunding", "Peras", "Sibuyas", "Apol"], correctAnswer: "Kalamunding" },
+            { id: 3, word: "Payung-payungan", image: fruitVeg3, sound: fruitVegS3, choices: ["Peras", "Sibuyas", "Payung-payungan", "Apol"], correctAnswer: "Payung-payungan" },
+            { id: 4, word: "Sibuyas", image: fruitVeg4, sound: fruitVegS4, choices: ["Sibuyas", "Payung-payungan", "Apol", "Saging"], correctAnswer: "Sibuyas" },
+            { id: 5, word: "Peras", image: fruitVeg5, sound: fruitVegS5, choices: ["Apol", "Saging", "Peras", "Ripolyu"], correctAnswer: "Peras" },
+            { id: 6, word: "Apol", image: fruitVeg6, sound: fruitVegS6, choices: ["Saging", "Ripolyu", "Apol", "Carrot"], correctAnswer: "Apol" },
+            { id: 7, word: "Saging", image: fruitVeg7, sound: fruitVegS7, choices: ["Ripolyu", "Carrot", "Saging", "Mayis"], correctAnswer: "Saging" },
+            { id: 8, word: "Ripolyu", image: fruitVeg8, sound: fruitVegS8, choices: ["Carrot", "Mayis", "Ripolyu", "Pinya"], correctAnswer: "Ripolyu" },
+            { id: 9, word: "Carrot", image: fruitVeg9, sound: fruitVegS9, choices: ["Mayis", "Pinya", "Carrot", "Papas"], correctAnswer: "Carrot" },
+            { id: 10, word: "Mayis", image: fruitVeg10, sound: fruitVegS10, choices: ["Pinya", "Papas", "Mayis", "Kulubasa"], correctAnswer: "Mayis" },
+            { id: 11, word: "Pinya", image: fruitVeg11, sound: fruitVegS11, choices: ["Papas", "Kulubasa", "Pinya", "Istroberi"], correctAnswer: "Pinya" },
+            { id: 12, word: "Papas", image: fruitVeg12, sound: fruitVegS12, choices: ["Kulubasa", "Istroberi", "Papas", "Kamatis"], correctAnswer: "Papas" },
+            { id: 13, word: "Kulubasa", image: fruitVeg13, sound: fruitVegS13, choices: ["Istroberi", "Kamatis", "Kulubasa", "Ubas"], correctAnswer: "Kulubasa" },
+            { id: 14, word: "Istroberi", image: fruitVeg14, sound: fruitVegS14, choices: ["Kamatis", "Ubas", "Istroberi", "Kalamunding"], correctAnswer: "Istroberi" },
+            { id: 15, word: "Kamatis", image: fruitVeg15, sound: fruitVegS15, choices: ["Ubas", "Kalamunding", "Kamatis", "Payung-payungan"], correctAnswer: "Kamatis" }
+        ],
+        genVocab: [
+            { id: 1, word: "Pagkeran", image: genVocab1, sound: genVocabS1, choices: ["Pagkeran", "Bisikleta", "Ayup", "Libru"], correctAnswer: "Pagkeran" },
+            { id: 2, word: "Bisikleta", image: genVocab2, sound: genVocabS2, choices: ["Bisikleta", "Ayup", "Libru", "Kandila"], correctAnswer: "Bisikleta" },
+            { id: 3, word: "Ayup", image: genVocab3, sound: genVocabS3, choices: ["Ayup", "Libru", "Kandila", "Kotsi"], correctAnswer: "Ayup" },
+            { id: 4, word: "Libru", image: genVocab4, sound: genVocabS4, choices: ["Libru", "Kandila", "Kotsi", "Asan"], correctAnswer: "Libru" },
+            { id: 5, word: "Kandila", image: genVocab5, sound: genVocabS5, choices: ["Kandila", "Kotsi", "Asan", "Bulaklak"], correctAnswer: "Kandila" },
+            { id: 6, word: "Kotsi", image: genVocab6, sound: genVocabS6, choices: ["Kotsi", "Asan", "Bulaklak", "Bale"], correctAnswer: "Kotsi" },
+            { id: 7, word: "Asan", image: genVocab7, sound: genVocabS7, choices: ["Asan", "Bulaklak", "Bale", "Pulo"], correctAnswer: "Asan" },
+            { id: 8, word: "Bulaklak", image: genVocab8, sound: genVocabS8, choices: ["Bulaklak", "Bale", "Pulo", "Llave"], correctAnswer: "Bulaklak" },
+            { id: 9, word: "Bale", image: genVocab9, sound: genVocabS9, choices: ["Bale", "Pulo", "Llave", "Diaryu"], correctAnswer: "Bale" },
+            { id: 10, word: "Pulo", image: genVocab10, sound: genVocabS10, choices: ["Pulo", "Llave", "Diaryu", "Lapis"], correctAnswer: "Pulo" },
+            { id: 11, word: "Llave", image: genVocab11, sound: genVocabS11, choices: ["Llave", "Diaryu", "Lapis", "Tanaman"], correctAnswer: "Llave" },
+            { id: 12, word: "Diaryu", image: genVocab12, sound: genVocabS12, choices: ["Diaryu", "Lapis", "Tanaman", "Bundok"], correctAnswer: "Diaryu" },
+            { id: 13, word: "Lapis", image: genVocab13, sound: genVocabS13, choices: ["Lapis", "Tanaman", "Bundok", "Tete"], correctAnswer: "Lapis" },
+            { id: 14, word: "Tanaman", image: genVocab14, sound: genVocabS14, choices: ["Tanaman", "Bundok", "Tete", "Boti"], correctAnswer: "Tanaman" },
+            { id: 15, word: "Bundok", image: genVocab15, sound: genVocabS15, choices: ["Bundok", "Tete", "Boti", "Pagkeran"], correctAnswer: "Bundok" },
+            { id: 16, word: "Tete", image: genVocab16, sound: genVocabS16, choices: ["Tete", "Boti", "Pagkeran", "Bisikleta"], correctAnswer: "Tete" },
+            { id: 17, word: "Boti", image: genVocab17, sound: genVocabS17, choices: ["Boti", "Pagkeran", "Bisikleta", "Ayup"], correctAnswer: "Boti" }
+        ],
+        
+    };
+
+    const pictureDictionary = pictureDictionaryByCategory[category] || [];
 
 
     const [questions, setQuestions] = useState([]);
